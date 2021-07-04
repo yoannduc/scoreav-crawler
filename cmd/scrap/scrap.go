c1825563b0b8aacea7617b0bc84e010569b79d6e
yoannduc <dev.yoannduc@gmail.com>
2021-07-04 23:57:17 +0200

    Implement by source format

--- cmd/scrap/scrap.go
+++ cmd/scrap/scrap.go
@@ -13,61 +13,104 @@ import (
 )
 
 type element struct {
-	ID    string `json:"id"`
-	Link  string `json:"link"`
-	Title string `json:"title"`
-	Date  string `json:"date"`
+	Pk     string `json:"pk"`
+	Sk     string `json:"sk"`
+	ID     string `json:"id"`
+	Link   string `json:"link"`
+	Title  string `json:"title"`
+	SDesc  string `json:"short_description"`
+	LDesc  string `json:"long_description,omitempty"`
+	Date   string `json:"date"`
+	Source string `json:"source"`
+	Type   string `json:"type"`
 }
 
-type event struct{}
+type event struct {
+	Source string `json:"source"`
+	Type   string `json:"type"`
+}
+
+func buildUri(e event) (string, error) {
+	switch e.Source {
+	case "scoreav":
+		switch e.Type {
+		case "news":
+			return "http://www.scoreav.com/news/", nil
+		case "focus":
+			return "http://www.scoreav.com/chronique/", nil
+		default:
+			return "", errors.New("Type empty or not handled for source " + e.Source + " (inputed: " + e.Type + ")")
+		}
+	default:
+		return "", errors.New("Source not handled or empty (inputed:\"" + e.Source + "\")")
+	}
+}
+
+func handleErr(e string) (string, error) {
+	log.Println(e)
+	return "", errors.New(e)
+}
+
+func buildElemList(d *goquery.Document, ev event) []*element {
+	var el []*element
+
+	switch ev.Source {
+	case "scoreav":
+		d.Find("article").Each(func(_ int, s *goquery.Selection) {
+			id := uuid.NewString()
+			l, _ := s.Find(".cb-post-title>a").Attr("href")
+			d, _ := s.Find(".cb-date>time").Attr("datetime")
+
+			e := &element{
+				Pk:     ev.Source,
+				Sk:     ev.Type + "#" + id,
+				ID:     id,
+				Link:   l,
+				Title:  s.Find(".cb-post-title").Text(),
+				SDesc:  s.Find(".cb-excerpt").Text(),
+				Date:   d,
+				Source: ev.Source,
+				Type:   ev.Type,
+			}
+
+			el = append(el, e)
+		})
+	}
+
+	return el
+}
 
 // HandleRequest handles the lambda job
 // It scraps scoreav site to get articles of today which are not yet in ddb
 // ctx is the lambda context
 // e is the event input
 func HandleRequest(ctx context.Context, e event) (string, error) {
-	log.Println("Hello World !")
-
-	var el []*element
-
-	resp, err := http.Get("http://www.scoreav.com/news/")
-	// resp, err := http.Get("http://www.scoreav.com/chronique/")
+	uri, err := buildUri(e)
 	if err != nil {
-		log.Println(errors.New("lul 1"))
-		return "", err
+		return handleErr(err.Error())
+	}
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		return handleErr(err.Error())
 	}
 	defer resp.Body.Close()
 
 	d, err := goquery.NewDocumentFromResponse(resp)
 	if err != nil {
-		log.Println(errors.New("lul 2"))
-		return "", err
+		return handleErr(err.Error())
 	}
 
-	log.Println(d)
-
-	d.Find("article").Each(func(_ int, s *goquery.Selection) {
-		l, _ := s.Find(".cb-post-title>a").Attr("href")
-		d, _ := s.Find(".cb-date>time").Attr("datetime")
-
-		e := &element{ID: uuid.NewString(), Link: l, Title: s.Find(".cb-post-title").Text(), Date: d}
-
-		log.Println(e)
-
-		el = append(el, e)
-	})
-
-	log.Println(el)
+	el := buildElemList(d, e)
 
 	json, err := jsoniter.MarshalToString(el)
 	if err != nil {
-		log.Println(errors.New("lul 3"))
-		return "", err
+		return handleErr(err.Error())
 	}
 
 	log.Println(json)
 
-	return "", nil
+	return json, nil
 }
 
 func main() {
